2aceae3a42c60229417a7a54db4fface1deb1d63
anoop <anoopaathreya@gmail.com>
2022-12-26 15:00:09 +0530

    config updated

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,14 +3,14 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_ME,
-//     appId: process.env.REACT_APP_ID
-// }
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_ME,
+    appId: process.env.REACT_APP_ID
+}
 
 
 
